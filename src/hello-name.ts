a5fd5b1cf7b1d2994308760365f78e1eed07b85a
Yushi Zhang <yushi.zhang@aspentech.com>
2024-12-31 10:57:25 +0800

    hello world

---
+++ src/hello-name.ts
@@ -0,0 +1,19 @@
+export class helloName{
+    name = "Narcissus"
+    value = 'test value'
+
+    firstName = '.bind firstName'
+    middleName = '.from-view middleName'
+    lastName = '.two-way lastName'
+
+    firstName2 = '.bind firstName'
+    middleName2 = '.from-view middleName'
+    lastName2 = '.two-way lastName'
+
+    imageSrc = '/favicon.ico'
+    altValue = 'img alt'
+
+    disableButton = true
+
+    content = '<h1>I am H1</h1>'
+}
